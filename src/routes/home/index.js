9ebea848762eea419c3752f2f4f5be42f0baa391
rivercome <897971480@qq.com>
2018-03-21 22:15:44 +0800

    完善静态页面

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,9 @@
 import React, { PureComponent } from 'react'
-import Item from './Item'
 import { immutableRenderDecorator } from 'react-immutable-render-mixin'
 import routerPush from 'utils/routerPush'
-import {Button} from 'antd'
-import {fromJS} from 'immutable'
-import {namesMap} from 'routerForm'
-const {HomePage2} = namesMap
+import { Input } from 'antd'
+import { namesMap } from 'routerForm'
+import './index.css'
 
 @routerPush
 @immutableRenderDecorator
@@ -13,26 +11,52 @@ export default class HomePage extends PureComponent {
   constructor (props) {
     super(props)
     this.state = {
-      count: fromJS({
-        value: 0
-      })
+      value: 0
     }
-  }
-
-  add = () => {
-    this.setState({
-      count: this.state.count.update('value', (value) => value + 1)
-    })
-    this.props.routerPush(HomePage2, {query: {a: [1, 2, 3]}})
   }
 
   render () {
     return (
-      <div onClick={this.add}>
-        edit 'src/routes/home/index.js' to start
-        <Button>点击</Button>
-        <Item count={this.state.count} />
+      <div>
+        <div className="first-title">
+          > 系统消息
+        </div>
+        <div className="page-content"> 通知公告<Input placeholder="value" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+        <div className="page-content"> 暂无消息<Input placeholder="default size" style={{width: 400, marginLeft: 20}} />
+        </div>
+
+        <div className="first-title">
+          > 服务网点概况
+        </div>
+        <div className="map-show">
+          百度地图调用
+        </div>
+        <div className="depart-server">
+          <div className="first-title">
+            > 项目服务概况
+          </div>
+          echarts
+        </div>
+        <div className="first-title">
+         >  风云榜
+        </div>
+          <div className="echarts-showlist">
+          </div>
       </div>
+
     )
   }
 }
